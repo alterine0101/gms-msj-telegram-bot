86bb45bbe88ba370aeac76db53cad74738e08f3e
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2023-03-18 16:04:40 +0700

    fix: Attendance

--- lib/wizards/attendanceGeneratorConversation.ts
+++ lib/wizards/attendanceGeneratorConversation.ts
@@ -6,7 +6,7 @@ import { InlineKeyboardButton } from "grammy/out/types.node";
 import parsePhoneNumber from "libphonenumber-js";
 import temp from "temp";
 import * as XLSX from "xlsx";
-import { COLUMN_ATTENDANCE_EMAIL, COLUMN_ATTENDANCE_NAME, COLUMN_ATTENDANCE_PHONE, COLUMN_ATTENDANCE_TIMESTAMP, COLUMN_PARTICIPANT_EMAIL, COLUMN_PARTICIPANT_ID, COLUMN_PARTICIPANT_NAME, COLUMN_PARTICIPANT_PHONE, COLUMN_PARTICIPANT_REMEDIAL } from "../constants";
+import { COLUMN_ATTENDANCE_EMAIL, COLUMN_ATTENDANCE_NAME, COLUMN_ATTENDANCE_PHONE, COLUMN_ATTENDANCE_TIMESTAMP, COLUMN_PARTICIPANT_EMAIL, COLUMN_PARTICIPANT_NAME, COLUMN_PARTICIPANT_PHONE } from "../constants";
 import { MyContext } from "..";
 
 const sanitize = (text: any) => typeof text == "string" ? text.replace(/;/g, "\\\;") : text;
@@ -122,7 +122,7 @@ export default async function attendanceGeneratorConversation(conversation: Conv
     return;
   }
 
-  await ctx.reply("Baik, silakan upload file data peserta Anda dalam format *XLSX*, *XLS* *ODT*, *NUMBERS*, maupun *CSV*\\.", {
+  await ctx.reply("Baik, silakan upload file *Data Peserta* Anda dalam format *XLSX*, *XLS* *ODT*, *NUMBERS*, maupun *CSV*\\.", {
     parse_mode: "MarkdownV2",
     reply_markup: {
       inline_keyboard: [[{
@@ -143,12 +143,10 @@ export default async function attendanceGeneratorConversation(conversation: Conv
       await resCtx.reply("Mohon maaf, pastikan Anda membalas pesan ini dengan file dokumen yang sudah disiapkan.");
     } else {
       try {
-        await conversation.external(async () => {
-          const file: FileX = await resCtx.getFile();
-          const path: string = await file.download();
-          const buffer: Buffer = await (await fs.open(path)).readFile();
-          wb = XLSX.read(buffer);
-        });
+        const file: FileX = await resCtx.getFile();
+        const path: string = await file.download();
+        const buffer: Buffer = await (await fs.open(path)).readFile();
+        wb = XLSX.read(buffer);
         isDocumentReceived = true;
       } catch (e) {
         console.error(e);
@@ -192,7 +190,7 @@ export default async function attendanceGeneratorConversation(conversation: Conv
   }
 
   const ws = wb!.Sheets[wb!.SheetNames[selectedSheet || 0]];
-  console.log(ws);
+  // console.log(ws);
   if (!ws.A1 || !ws.B1 || !ws.B2 || !ws.B3 || !ws.B4) {
     resCtx.reply("Mohon maaf, format spreadsheet Anda tidak valid. Pastikan Anda menggunakan template /templatepeserta untuk hasil yang terbaik.");
     return;
@@ -208,12 +206,12 @@ export default async function attendanceGeneratorConversation(conversation: Conv
     let isDocumentReceived = false;
     let wb: XLSX.WorkBook;
 
-    await resCtx.reply(`Baik, masukkan file rekaman *Absensi ${i+1}* Anda.`, {
+    await resCtx.reply(`Baik, masukkan file rekaman *Absensi ${i+1}* Anda\\.`, {
       parse_mode: "MarkdownV2",
       reply_markup: {
         inline_keyboard: (i < 1 ? [] : [[
           {
-            text: "➡️ Lewati Semua",
+            text: "➡️ Skip (Langsung Proses)",
             callback_data: "skip",
           }
         ]]).concat([[
@@ -224,36 +222,33 @@ export default async function attendanceGeneratorConversation(conversation: Conv
         ]])
       }
     });
-    if (i == 0) await resCtx.reply("Pastikan masing-masing file memiliki urutan kolom sebagai berikut:\n\n1. Timestamp\n2.Nama Lengkap\n3. Tanggal Lahir\n4. Nomor HP\n5. Email\n6. NIJ (Khusus MSJ 2 dan 3)");
-    do {
-      resCtx = await conversation.wait() as MyContext;
-      if (resCtx.update && resCtx.update.callback_query) {
-        switch (resCtx.update.callback_query.data) {
-          case "cancel":
-            await resCtx.reply("Operasi dibatalkan.");
-            return;
-          default:
-            done = true;
-            break;
-        }
-      } else if (resCtx.message != null && resCtx.message.document == null) {
-        await resCtx.reply("Mohon maaf, pastikan Anda membalas pesan ini dengan file dokumen yang sudah disiapkan.");
-      } else {
-        try {
-          await conversation.external(async () => {
-            const file: FileX = await resCtx.getFile();
-            const path: string = await file.download();
-            const buffer: Buffer = await (await fs.open(path)).readFile();
-            wb = XLSX.read(buffer);
-          });
-          isDocumentReceived = true;
-        } catch (e) {
-          console.error(e);
-          resCtx.reply("Mohon maaf, kami sedang mengalami kendala dalam memproses file Anda.");
+
+    if (i == 0) await resCtx.reply("Pastikan masing-masing file memiliki urutan kolom sebagai berikut:\n\n1. Timestamp\n2.Nama Lengkap\n3. Tanggal Lahir\n4. Email\n5. Nomor HP\n6. NIJ (Khusus MSJ 2 dan 3)");
+    const resCtx2 = await conversation.wait() as MyContext;
+    console.log(resCtx2);
+    if (resCtx2.update && resCtx2.update.callback_query) {
+      switch (resCtx2.update.callback_query.data) {
+        case "cancel":
+          await resCtx2.reply("Operasi dibatalkan.");
           return;
-        }
+        default:
+          continue;
       }
-    } while (!isDocumentReceived);
+    } else if (resCtx2.message != null && resCtx2.message.document == null) {
+      await resCtx2.reply("Mohon maaf, pastikan Anda membalas pesan ini dengan file dokumen yang sudah disiapkan.");
+    } else {
+      try {
+        const file: FileX = await resCtx2.getFile();
+        const path: string = await file.download();
+        const buffer: Buffer = await (await fs.open(path)).readFile();
+        wb = XLSX.read(buffer, { cellDates: true });
+        isDocumentReceived = true;
+      } catch (e) {
+        console.error(e);
+        resCtx2.reply("Mohon maaf, kami sedang mengalami kendala dalam memproses file Anda.");
+        return;
+      };
+    };
 
     let selectedSheet: number|null = null;
     if (wb!.SheetNames.length <= 0) {
@@ -286,16 +281,16 @@ export default async function attendanceGeneratorConversation(conversation: Conv
           }
         }
       } while (selectedSheet == null || Number.isNaN(selectedSheet) || selectedSheet! < 0 || selectedSheet! > wb!.SheetNames.length);
-
-      const ws = wb!.Sheets[wb!.SheetNames[selectedSheet || 0]];
-      console.log(ws);
-      if (!ws.A1 || !ws.B1 || !ws.C1 || !ws.D1 || !ws.E1 || (parseInt(msjType) > 1 && !ws.F1)) {
-        resCtx.reply("Mohon maaf, format spreadsheet Anda tidak valid.");
-        return;
-      }
-
-      data[i + 1] = XLSX.utils.sheet_to_json(ws, { header: 1 });
     }
+    const ws = wb!.Sheets[wb!.SheetNames[selectedSheet || 0]];
+    console.log(ws);
+    if (!ws.A1 || !ws.B1 || !ws.C1 || !ws.D1 || !ws.E1 || (parseInt(msjType) > 1 && !ws.F1)) {
+      resCtx.reply("Mohon maaf, format spreadsheet Anda tidak valid.");
+      return;
+    }
+
+    data[i + 1] = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    // await resCtx.reply(data[i + 1].length.toString());
   }
 
   await resCtx.reply("Pilih format file yang akan dibuat", {
@@ -310,8 +305,6 @@ export default async function attendanceGeneratorConversation(conversation: Conv
             text: "ODS",
             callback_data: "ods",
           },
-        ],
-        [
           {
             text: "XLS",
             callback_data: "xls",
@@ -341,16 +334,16 @@ export default async function attendanceGeneratorConversation(conversation: Conv
   if (data[3]) finalColumns.concat(["Waktu Absen 3", "Kecocokan Data Absen 3"]);
   data.final = [finalColumns];
 
-  function searchTimestamp(participant: Array<string|number|boolean|null>, no: number): [string|null, string|null] {
-    let current = data[no] as Array<Array<string|number|boolean>>;
+  function searchTimestamp(participant: Array<string|number|boolean|Date|null>, no: number): [string|null, string|null] {
+    let current = data[no] as Array<Array<string|number|boolean|Date|null>>;
     for (let i = 1; i < current.length; i++) {
       let matches: string[] = [];
-      if (current[i][COLUMN_ATTENDANCE_NAME].toString().toLowerCase().replace(/\s+/, " ") == participant[COLUMN_PARTICIPANT_NAME]!.toString().toLowerCase().replace(/\s+/, " ")) matches.push("Nama");
-      if (participant[COLUMN_PARTICIPANT_EMAIL] != null && current[i][COLUMN_ATTENDANCE_EMAIL].toString().toLowerCase() == participant[COLUMN_PARTICIPANT_EMAIL]!.toString().toLowerCase()) matches.push("Email");
-      if (participant[COLUMN_PARTICIPANT_PHONE] != null && parsePhoneNumber(current[i][COLUMN_ATTENDANCE_PHONE].toString(), "ID") == parsePhoneNumber(participant[COLUMN_PARTICIPANT_PHONE].toString(), "ID")) matches.push("Nomor Telepon");
+      if (current[i][COLUMN_ATTENDANCE_NAME]!.toString().toLowerCase().replace(/\s+/, " ") == participant[COLUMN_PARTICIPANT_NAME]!.toString().toLowerCase().replace(/\s+/, " ")) matches.push("Nama");
+      if (current[i][COLUMN_ATTENDANCE_EMAIL] && participant[COLUMN_PARTICIPANT_EMAIL] && current[i][COLUMN_ATTENDANCE_EMAIL]!.toString().toLowerCase() == participant[COLUMN_PARTICIPANT_EMAIL].toString().toLowerCase()) matches.push("Email");
+      if (current[i][COLUMN_ATTENDANCE_PHONE] && participant[COLUMN_PARTICIPANT_PHONE] && parsePhoneNumber(current[i][COLUMN_ATTENDANCE_PHONE]!.toString(), "ID") == parsePhoneNumber(participant[COLUMN_PARTICIPANT_PHONE].toString(), "ID")) matches.push("Nomor Telepon");
 
-      if (matches.length > 0) return [current[i][COLUMN_ATTENDANCE_TIMESTAMP].toString(), matches.join(",")];
-      
+      if (matches.length > 0) return [(current[i][COLUMN_ATTENDANCE_TIMESTAMP] as Date).toISOString(), matches.join(", ")];
+
       /* Check for similar names */
       let nameSplit1 = participant[COLUMN_PARTICIPANT_NAME]!.toString().toLowerCase().split(/\s+/).sort();
       let nameHash1: Map<string, number> = new Map();
@@ -374,34 +367,41 @@ export default async function attendanceGeneratorConversation(conversation: Conv
   }
 
   for (let i = 1; i < data.participants.length; i++) {
+    // console.log(data.participants[i]);
+    // await resCtx.reply(data.participants[i].toString());
     let insert = (data.participants[i] as Array<string|number|boolean|null>).splice(0, 5);
     
-    insert.concat(searchTimestamp(insert, 1));
-    if (data[2]) insert.concat(searchTimestamp(insert, 2));
-    if (data[3]) insert.concat(searchTimestamp(insert, 3));
+    // await resCtx.reply(data[1].length.toString());
+    insert = insert.concat(searchTimestamp(insert, 1));
+    if (data[2]) insert = insert.concat(searchTimestamp(insert, 2));
+    if (data[3]) insert = insert.concat(searchTimestamp(insert, 3));
+
+    data.final.push(insert);
   }
 
   const now = new Date();
   let resSheetNames = ["Rekap Absensi"];
   if (resFormat != "csv" && warnings.length > 0) resSheetNames.push(`Warnings (${warnings.length})`);
-  const resTitle = `Absensi MSJ ${msjType} Sesi ${session} ${now.getFullYear}-${now.getMonth}-${now.getDate}`;
+  const resTitle = `Absensi MSJ ${msjType} Sesi ${session} ${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
   let resWb = XLSX.utils.book_new();
   resWb.Props = {
     Application: "GMS MSJ Admin Bot",
     AppVersion: "1.2.0",
     CreatedDate: now,
-    SheetNames: resSheetNames,
     Title: resTitle,
   };
-  resWb.Sheets[resSheetNames[0]] = XLSX.utils.aoa_to_sheet(data.final);
+  XLSX.utils.book_append_sheet(resWb, XLSX.utils.aoa_to_sheet(data.final), resSheetNames[0]);
   if (warnings.length > 0) {
     if (resFormat != "csv"){
-      resWb.Sheets[resSheetNames[1]] = XLSX.utils.aoa_to_sheet(([["No", "Catatan"]] as Array<string|number>[]).concat(warnings.map((value, index) => [index + 1, value])));
+      XLSX.utils.book_append_sheet(resWb, XLSX.utils.aoa_to_sheet(([["No", "Catatan"]] as Array<string|number>[]).concat(warnings.map((value, index) => [index + 1, value]))), resSheetNames[1]);
     } else {
       await resCtx2.reply(["⚠️ Warning!"].concat(warnings).join("\n"));
     }
   }
 
-  const u8 = XLSX.write(resWb, { bookType: resFormat as XLSX.BookType });
+  const u8 = XLSX.write(resWb, {
+    bookType: resFormat as XLSX.BookType,
+    type: "buffer",
+  });
   resCtx2.replyWithDocument(new InputFile(u8, `${resTitle}.${resFormat}`));
 }
