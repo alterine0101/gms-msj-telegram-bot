2eb1bc68391f495c2722410c931d6ba81726845e
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2023-09-07 13:59:41 +0700

    try: (#_ ): mitigate corrupted CSV bug

--- lib/wizards/contactListGeneratorConversation.ts
+++ lib/wizards/contactListGeneratorConversation.ts
@@ -77,7 +77,7 @@ export default async function contactListGeneratorConversation(conversation: Con
       }
     });
 
-    const resCtx2 = await conversation.waitFrom(ctx.user);
+    const resCtx2 = await conversation.wait();
     if (resCtx2.update?.callback_query?.data == "cancel") {
       await resCtx.reply("Operasi dibatalkan.");
       return;
